69e25f0532cf1024b64f65e04e6596b9f2ee1458
S Rafay <sRafay2021@gmail.com>
2024-11-08 23:08:41 +0500

    Push My Calculator

---
+++ app.js
@@ -0,0 +1,28 @@
+
+// Get the display element
+const display = document.getElementById('display');
+
+// Function to clear the display
+function clearDisplay() {
+    display.value = '';
+}
+
+// Function to delete the last character
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Function to append to display
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+// Function to calculate the result
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+
+}
